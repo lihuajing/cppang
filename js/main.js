d0a955a78754e1520c2389d6d5816d3b3ee2b60c
lihuajing <lihuajing@126.com>
2015-08-01 23:09:56 +0800

    add bootstrap less

--- js/main.js
+++ js/main.js
@@ -1,31 +1,47 @@
 $(document).ready(function () {
-    $('.carousel').carousel({interval: 3000});
-    var upArrow = $('<span class="glyphicon glyphicon-arrow-up"></span>');
-    var downArrow = $('<span class="glyphicon glyphicon-arrow-down"></span>');
-    for (var i = 0; i < 2; i++) {
-        var x = Math.random();
-        var n = i + 1;
-        var domA = $('#a' + n);
-        var domB = $('#b' + n);
-        var domC = $('#c' + n);
-        var domAn = domA.next();
-        var domBn = domB.next();
-        var domCn = domC.next();
-
-        if (domAn.hasClass('glyphicon')) {
-            domAn.remove();
-        }
-        x > 0.5 ? domA.after(upArrow) : domA.after(downArrow);
-
-        if (domBn.hasClass('glyphicon')) {
-            domBn.remove();
-        }
-        x > 0.5 ? domB.after(upArrow) : domB.after(downArrow);
-
-        if (domCn.hasClass('glyphicon')) {
-            domCn.remove();
-        }
-        x > 0.5 ? domC.after(upArrow) : domC.after(downArrow);
-
-    }
+    var dragElement = $('#table1'),
+        dropElement = $('#smallContainer');
+    dragElement.draggable({
+        addClasses: true,
+        //appendTo: '#bigContainer',
+        //helper: 'clone',
+        //axis:'x',
+        //cancel: 'div',
+        //connectToSortable:'#smallContainer',
+        //containment: '#smallContainer',
+        cursor: 'pointer',
+        //cursorAt: {right: 50},
+        //delay:1000,
+        //disabled:true,
+        //distance:20,
+        //grid: [10, 10],
+        //handle:'#dragContainer',
+        //iframeFix:true,
+        //opacity: 0.5,
+        //refreshPositions: false,
+        revert:'invalid',
+        //revertDuration:3000,
+        //scope:'putHere',
+        //scroll: true,
+        //scrollSensitivity: 200,
+        //scrollSpeed:50,
+        //snap: true,
+        //snapMode:'inner',
+        //snapTolerance:10,
+        //stack:'#table5',
+        //zIndex:100,
+    });
+    //$('#table1').draggable('destroy');
+    //$('#table1').draggable('disable');
+    //$('#table1').draggable('enable');
+    //var ins=$('#table1').draggable('instance');
+    //var options=$('#table1').draggable('option','cursorAt.right');
+    //var options=$('#table1').draggable('option');
+    //$('#table1').draggable('option','scroll',false);
+    //$('#table1').draggable('option',{'scroll':false,'snap':false});
+    //var widget = $("#table1").draggable("widget");
+    //dragElement.bind('dragstop', function (event, ui) {
+    //    ui.helper.clone().appendTo(dropElement)
+    //});
+    dropElement.droppable();
 });
